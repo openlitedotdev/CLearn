13b513eb567eafe2cb44e3da3197d6666d7093d6
sebastiandotdev <sebastiandotdev@gmail.com>
2025-03-12 22:28:39 -0500

    chore: setup eslint powered by `@antfu/eslint-config`

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,17 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-
+  future: {
+    compatibilityVersion: 4,
+  },
+  eslint: {
+    config: { standalone: false },
+  },
   modules: [
     '@nuxt/content',
     '@nuxt/eslint',
     '@nuxt/fonts',
     '@nuxt/icon',
-    '@nuxt/image'
-  ]
+    '@nuxt/image',
+  ],
 })
