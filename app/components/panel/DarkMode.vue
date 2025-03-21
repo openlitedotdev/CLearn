b9498620bb892d3006ad2999fb2762d10889ae46
Sebastián García <sebastiandotdev@gmail.com>
2025-03-20 23:09:28 -0500

    feat: support dark mode powered by vueuse (#13)

---
+++ app/components/panel/DarkMode.vue
@@ -0,0 +1,13 @@
+<template>
+  <ClientOnly>
+    <button
+      h-10 w-10 rounded-full hover:bg-active
+      hover="bg-active"
+      flex="~ items-center justify-center"
+      @click="toggleDark()"
+    >
+      <Icon v-if="!isDark" name="solar:moon-bold" />
+      <Icon v-else name="solar:sun-bold" />
+    </button>
+  </ClientOnly>
+</template>
--- uno.config.ts
+++ uno.config.ts
@@ -1,6 +1,17 @@
 import { defineConfig, presetAttributify, presetTypography, presetWind3 } from 'unocss'
 
 export default defineConfig({
+  shortcuts: {
+    'border-base': 'border-gray-200 dark:border-gray-800',
+    'bg-active': 'bg-gray:10',
+    'bg-faded': 'bg-gray:5',
+    'bg-code': 'bg-gray/5',
+    'dropdown': 'relative',
+    'dropdown-content': 'my-1 min-w-[120px] rounded bg-white p-0 py-2 text-black shadow dark:bg-black dark:text-white',
+    'dropdown-item': 'flex items-center px-4 py-2 hover:bg-faded hover:bg-active',
+    'panel': 'relative rounded-md bg-white p-3 shadow dark:bg-[#0e1726]',
+    'center': 'flex items-center justify-center',
+  },
   presets: [
     presetAttributify(),
     presetTypography(),
